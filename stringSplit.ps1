ace3aae14a026a0e80ace88b475cc2a57ef6641d
chucklapress <charleslapress@gmail.com>
2020-01-24 15:26:14 -0500

    missed Process for each in math function

--- stringSplit.ps1
+++ stringSplit.ps1
@@ -1,2 +1,2 @@
 $weirdneil = "Hey, hey, my, my. Out of the blue and into the black, You pay for this and they give you that. Once you're gone you can't come back, When you're out of the blue and into the black"
-$weirdneil | ForEach-Object {$_.Split(" ")}
+$weirdneil | ForEach-Object -Process {$_.Split(" ")}
