e219c382f9b43d08e23c25615276d37a4f4b6ef2
chucklapress <charleslapress@gmail.com>
2020-01-26 11:14:28 -0500

    powershell wordcount

---
+++ wordCount.ps1
@@ -0,0 +1,13 @@
+#powershell to strip and examine a file
+$words = "Ira Hayes. Call him drunken Ira Hayes, He won't answer anymore, Not the whiskey drinking Indian, Or the marine that went to war. Gather 'round me people, There's a story I would tell 'Bout a brave young Indian You should remember well From the land of the Pima Indian, A proud and noble band, Who farmed the Phoenix Valley In Arizona land."
+$words = $words.ToLower() -replace "[.,!?)(]",""
+$words = $words -replace "([^a-z])'([^a-z])","`${1}`${2}"
+$words = $words -replace "'","''"
+$words | Measure-Object -Character -Line -Word
+
+<# expected output
+Lines Words Characters Property
+----- ----- ---------- --------
+    1    62        334
+#>
+Write-Host "Man in black"
