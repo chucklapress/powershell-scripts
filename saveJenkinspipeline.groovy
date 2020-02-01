3b0a3e1e493178913177eb51319f8466c327e4e1
chucklapress <charleslapress@gmail.com>
2020-02-01 12:03:29 -0500

    modified jenkins pipeline script

--- saveJenkinspipeline.groovy
+++ saveJenkinspipeline.groovy
@@ -8,11 +8,11 @@ stage ('One') {
 }
 stage ('Two') {
     node ('masterserver') {
-        pwsh(script: 'Get-Date -Format "dddd MM/dd/yyyy HH:mm')
+        pwsh(script: 'Get-Date -Format "dddd MM/dd/yyyy HH:mm"')
     }
 }
 stage ('Three') {
     node ('masterserver') {
-        pwsh(script: 'Move-Item /Users/chucklapress/test1.txt -Destination /Users/chucklapress/powershell_scripts/usefulScripts/tst1.txt -Force')
+        pwsh(script: '"happy people eat spicy pho for lunch" | Measure-Object -Character -Line -Word | Out-File -FilePath /Users/chucklapress/temps/OUTPUT.txt')
     }
 }
